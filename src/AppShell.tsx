59e034b1cf47685621a1bcd372d57ea4b4027767
nramapurath <nramapurath@gmail.com>
2023-05-28 19:54:43 +0800

    Add links in sidebar

--- src/AppShell.tsx
+++ src/AppShell.tsx
@@ -10,65 +10,80 @@ import {
   Burger,
   useMantineTheme,
 } from "@mantine/core";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 
 export default function AppShellPage() {
   const theme = useMantineTheme();
   const [opened, setOpened] = useState(false);
   return (
-    <AppShell
-      styles={{
-        main: {
-          background:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[8]
-              : theme.colors.gray[0],
-        },
-      }}
-      navbarOffsetBreakpoint="sm"
-      asideOffsetBreakpoint="sm"
-      navbar={
-        <Navbar
-          p="md"
-          hiddenBreakpoint="sm"
-          hidden={!opened}
-          width={{ sm: 200, lg: 300 }}
-        >
-          <Text>Application navbar</Text>
-        </Navbar>
-      }
-      aside={
-        <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
-          <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
-            <Text>Application sidebar</Text>
-          </Aside>
-        </MediaQuery>
-      }
-      footer={
-        <Footer height={60} p="md">
-          Application footer
-        </Footer>
-      }
-      header={
-        <Header height={{ base: 50, md: 70 }} p="md">
-          <div
-            style={{ display: "flex", alignItems: "center", height: "100%" }}
+    <Router>
+      <AppShell
+        styles={{
+          main: {
+            background:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
+          },
+        }}
+        navbarOffsetBreakpoint="sm"
+        asideOffsetBreakpoint="sm"
+        navbar={
+          <Navbar
+            p="md"
+            hiddenBreakpoint="sm"
+            hidden={!opened}
+            width={{ sm: 200, lg: 300 }}
           >
-            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-              <Burger
-                opened={opened}
-                onClick={() => setOpened((o) => !o)}
-                size="sm"
-                color={theme.colors.gray[6]}
-                mr="xl"
-              />
-            </MediaQuery>
+            <Navbar.Section grow mt="lg">
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <Text component={Link} variant="link" to="/">
+                  Home Page
+                </Text>
+                <Text component={Link} variant="link" to="/chat">
+                  Chat
+                </Text>
+                <Text component={Link} variant="link" to="/findMentor">
+                  Select mentor
+                </Text>
+              </div>
+            </Navbar.Section>
+          </Navbar>
+        }
+        aside={
+          <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
+            <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
+              <Text>Application sidebar</Text>
+            </Aside>
+          </MediaQuery>
+        }
+        footer={
+          <Footer height={60} p="md">
+            Application footer
+          </Footer>
+        }
+        header={
+          <Header height={{ base: 50, md: 70 }} p="md">
+            <div
+              style={{ display: "flex", alignItems: "center", height: "100%" }}
+            >
+              <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                <Burger
+                  opened={opened}
+                  onClick={() => setOpened((o) => !o)}
+                  size="sm"
+                  color={theme.colors.gray[6]}
+                  mr="xl"
+                />
+              </MediaQuery>
 
-            <Text>Application header</Text>
-          </div>
-        </Header>
-      }
-    >
-      <Text>Resize app to see responsive navbar in action</Text>
-    </AppShell>
+              <Text>Application header</Text>
+            </div>
+          </Header>
+        }
+      >
+        <Text>Resize app to see responsive navbar in action</Text>
+      </AppShell>
+    </Router>
   );
 }
