281f566bd1a27201f4efafc6adf1b1efde00558a
nramapurath <nramapurath@gmail.com>
2023-05-29 21:42:51 +0800

    Add login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,97 +1,61 @@
-import React from "react";
+"use client";
 
-import Image from "next/image";
 import styles from "../page.module.css";
 
-export default function Login() {
+import { useState } from "react";
+import { useForm } from "@mantine/form";
+import { Button, TextInput, Box } from "@mantine/core";
+
+import url from "../../../utils/url";
+
+const postLogin = async (values: { email: string; password: string }) => {
+  const formInput = {
+    email: values.email,
+    password: values.password,
+  };
+  const response = await fetch(url + "/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formInput),
+  });
+  const data = await response.json();
+  if (data.token) {
+    console.log(data.token);
+    console.log(data.user);
+    localStorage.setItem("token", "Bearer " + data.token);
+    localStorage.setItem("user", JSON.stringify(data.user));
+  }
+};
+
+export default function login() {
+  const form = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // States for checking error
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(false);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <Box maw={320} mx="auto">
+      <form onSubmit={form.onSubmit((values) => postLogin(values))}>
+        <TextInput
+          label="Email"
+          placeholder="Email"
+          {...form.getInputProps("email")}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <TextInput
+          label="Password"
+          placeholder="Password"
+          {...form.getInputProps("password")}
+        />
+        <Button type="submit">Login</Button>
+      </form>
+    </Box>
   );
 }
