2eb2407627ec72f0d47c246d969bdb65e6f33b51
kxrt <kvrtikeya@gmail.com>
2023-05-30 00:23:11 +0800

    feat: create landing page

---
+++ src/app/landing/header.tsx
@@ -0,0 +1,48 @@
+import { createStyles, Header, Container, Image, rem } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  header: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: theme.spacing.md,
+  },
+  title: {
+    display: "flex",
+    flexDirection: "row",
+    fontWeight: 800,
+    fontSize: rem(28),
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+
+    [theme.fn.smallerThan("xs")]: {
+      textAlign: "left",
+    },
+  },
+  titleHighlight: {
+    fontWeight: 800,
+    fontSize: rem(28),
+    color:
+      theme.colors[theme.primaryColor][theme.colorScheme === "dark" ? 4 : 6],
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+
+    [theme.fn.smallerThan("xs")]: {
+      textAlign: "left",
+    },
+  },
+}));
+
+export function HeaderSimple() {
+  const { classes, cx } = useStyles();
+
+  return (
+    <Header height={60} mb={60} display={"flex"}>
+      <Container className={classes.header}>
+        <Image src="./logo.svg" alt="Logo" height={28} width={28} />
+        <p className={classes.title}>
+          Alum<p className={classes.titleHighlight}>NUS</p>
+        </p>
+      </Container>
+    </Header>
+  );
+}
